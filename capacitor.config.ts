06a767e1243d6580cb125e0cc209b8bc864144a0
Andrew <akariaka@yandex.ru>
2026-04-20 02:02:04 +0600

    add feature moblie

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.akaryaka.todoapp',
+  appName: 'My Todo',
+  webDir: 'dist'
+};
+
+export default config;
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
 import { createApp } from 'vue'
 import './style.css'
+// import { Keyboard } from '@capacitor/keyboard'
 import App from './App.vue'
 
 createApp(App).mount('#app')
+
+// Keyboard.setScrollBehavior({ behavior: 'keyboard' }) // авто-скролл к активному полю
