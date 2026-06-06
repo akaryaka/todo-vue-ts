3a53d07ee7ec7535d0f0aa8be87d260128a9d7eb
akariaka@yandex.ru <akariaka@yandex.ru>
2026-06-06 18:52:08 +0600

    add config pwa-plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,61 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    VitePWA({
+      registerType: 'autoUpdate',   
+      manifest: {
+        name: 'Todo',
+        short_name: 'Todo',
+        description: 'Todo desc',
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+        display: 'standalone', // Убирает адресную строку браузера
+        start_url: '/',
+        icons: [
+          {
+            src: '/web-app-manifest-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/web-app-manifest-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: '/web-app-manifest-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable' // Важно для адаптивных иконок на Android
+          }
+        ]
+      },
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,jpg,svg,woff,woff2}'],
+        runtimeCaching: [
+          {
+            urlPattern: /^https:\/\/api\.yourdomain\.com\/.*/i,
+            handler: 'NetworkFirst',
+            options: {
+              cacheName: 'api-cache',
+              expiration: {
+                maxEntries: 50,
+                maxAgeSeconds: 60 * 60 * 24 // 1 день
+              },
+              cacheableResponse: {
+                statuses: [0, 200]
+              }
+            }
+          }
+        ]
+      }
+    })
+  ],
   server: {
     open: true,
   },
