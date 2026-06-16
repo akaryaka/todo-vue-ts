adec025652f584745f95553d740c4a3cb3018d9c
unknown <akariaka@yandex.ru>
2026-06-16 16:32:30 +0300

    rename project

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { VitePWA } from 'vite-plugin-pwa'
 
-const REPO_NAME = 'todo-vue-ts'; 
+const REPO_NAME = 'todo'; 
 // https://vite.dev/config/
 export default defineConfig({
   base: `/${REPO_NAME}/`,
